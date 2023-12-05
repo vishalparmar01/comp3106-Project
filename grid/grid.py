4cc85c280cae4d6d422fd93bc4940ededd11114a
Michael Bradley <michaelmbrad@gmail.com>
2023-12-05 01:59:35 -0500

    Change grid fill rate

--- grid/grid.py
+++ grid/grid.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from random import choice, randint
+from random import choice, random
 
 import numpy as np
 
@@ -21,19 +21,11 @@ def create_dynamic_grid(rows: int, columns: int):
     grid = np.zeros((rows, columns), dtype=np.uint8)
 
     bin_placed = False
-    total_cells = rows * columns
-    min_target_cells = total_cells * 0.65 # At least 1/2 of the grid should have trash
-    max_target_cells = total_cells * 0.75  # 3/4 or less of the grid should have trash
-
-    target_cells = randint(min_target_cells, max_target_cells)
-    trash_cells = 0
 
     for i in range(rows):
         for j in range(columns):
-            if trash_cells < target_cells and randint(0, 1):  # Randomly choose whether to place trash
-                trash_type = choice([Cell.WETTRASH, Cell.DRYTRASH, Cell.DUSTY, Cell.SOAKED]).value
-                grid[i, j] = trash_type
-                trash_cells += 1
+            if random() < 0.65:  # Randomly choose whether to place trash
+                grid[i, j] = choice([Cell.WETTRASH, Cell.DRYTRASH, Cell.DUSTY, Cell.SOAKED]).value
             elif not bin_placed:
                 grid[i, j] = Cell.BIN.value
                 bin_placed = True
@@ -43,10 +35,12 @@ def create_dynamic_grid(rows: int, columns: int):
 
     return grid
 
+
 def print_grid(grid):
     # Print the grid
     for row in grid:
         print(" ".join(map(str, row)))
+
 
 def get_start_positions(grid: np.ndarray) -> AgentLocations:
     return {
@@ -54,11 +48,3 @@ def get_start_positions(grid: np.ndarray) -> AgentLocations:
         AgentType.VACUUM: (1, 0),
         AgentType.MOP: (2, 0),
     }
-
-# Example usage
-# rows = int(input("Enter the number of rows: "))
-# columns = int(input("Enter the number of columns: "))
-
-# grid = create_dynamic_grid(rows, columns)
-#print("\nInitial Grid:")
-#print_grid(grid)
