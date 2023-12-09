ef4d3344b886de01938f0b97d35ca64e12b0523a
Michael Bradley <michaelmbrad@gmail.com>
2023-12-09 17:42:06 -0500

    Make repeated tests more consistent

--- sim/app.py
+++ sim/app.py
@@ -352,6 +352,7 @@ class Simulator(App[None]):
     def reset(self, change_seed: bool):
         self.logger.clear()
         if change_seed:
+            seed(self.seed)
             self.seed = random()
         seed(self.seed)
         self.motd()
